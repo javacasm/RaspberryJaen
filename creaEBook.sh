011b95190260b7e5b52bc8a738482fe682331119
javacasm <javacasm@gmail.com>
2016-11-30 00:25:56 +0100

    detalles

--- creaEBook.sh
+++ creaEBook.sh
@@ -5,7 +5,7 @@ TEMP_file=$TEMP_DIR"/ebook.md"
 rm -rf $TEMP_DIR
 mkdir $TEMP_DIR
 
-files=( historia.md clones.md proyectos.md raspiMedios.md iniciativasSimilares.md raspberryVSarduino.md OpenSource.md equipo.md dondeyquecomprar.md estructura.md sos.md instalacion.md problemas.md mantenimiento.md manejando.md usos.md simuladores.md Exteriores.md conexionDirecta.md programando.md scratch.md shell.md python.md aplicaciones.md apis.md openCV.md mathematicaPi.md electronica.md leds.md complementos.md documentacion.md RaspiFAQ.md )
+files=( historia.md clones.md proyectos.md raspiMedios.md iniciativasSimilares.md raspberryVSarduino.md OpenSource.md equipo.md dondeyquecomprar.md estructura.md sos.md instalacion.md problemas.md mantenimiento.md manejando.md conexionDirecta.md usos.md simuladores.md Exteriores.md conexionDirecta.md programando.md scratch.md shell.md python.md aplicaciones.md apis.md openCV.md mathematicaPi.md electronica.md leds.md complementos.md documentacion.md RaspiFAQ.md )
 
 #  "apis.md" "aplicaciones.md"  "arduino.md"  "clones.md"  "complementos.md"  "conexionDirecta.md"  "controlVoz.md"  "documentacion.md"  "dondeyquecomprar.md"  "electronica.md"  "encuestaInicial.md"  "equipo.md"  "estructura.md"  "Exteriores.md"  "hacking.md"  "hardware.md"  "historia.md"  "indice.md"  "iniciativasSimilares.md"  "instalacion.md"  "leds.md"  "manejando.md"  "mantenimiento.md"  "materiales.md"  "mathematicaPi.md"  "openCV.md"  "OpenSource.md"  "problemas.md"  "programando.md"  "proyectos.md"  "python.md"  "raspberryVSarduino.md"  "RaspiFAQ.md"  "raspiMedios.md"  "README.md"  "scratch.md"  "shell.md"  "simuladores.md"  "sos.md"  "usos.md"  "vigilancia.md")
 

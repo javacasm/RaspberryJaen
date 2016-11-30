d535ace07319a33ef89e7facd481d7c68367d5cd
javacasm <javacasm@gmail.com>
2016-11-30 16:07:35 +0100

    formato

--- creaEBook.sh
+++ creaEBook.sh
@@ -2,6 +2,11 @@
 
 TEMP_DIR="ebook-temp"
 TEMP_file=$TEMP_DIR"/ebook.md"
+TEMP_DownloadImagesFile=$TEMP_DIR"/get_imagenes.sh"
+TEMP_ImagesFile=$TEMP_DIR"/imagesURLS.txt"
+TEMP_DirImages=$TEMP_DIR"/images/"
+TEMP_DownloadImagesNames=$TEMP_DIR"/images.txt"
+
 rm -rf $TEMP_DIR
 mkdir $TEMP_DIR
 
@@ -14,13 +19,16 @@ files=( historia.md \
     OpenSource.md \
     equipo.md \
     dondeyquecomprar.md \
-    estructura.md sos.md \
+    estructura.md \
+    sos.md \
     instalacion.md \
     problemas.md \
     mantenimiento.md \
     manejando.md \
     conexionDirecta.md \
     usos.md \
+    hacking.md \
+    vigilancia.md \
     simuladores.md \
     Exteriores.md \
     programando.md \
@@ -44,7 +52,27 @@ do :
    grep -v  -E '@javacasm|Licencia_CC|CEP|Noviembre|juntadeandalucia|### \[https://github.com/javacasm/RaspberryJaen' $i >>$TEMP_file
 done
 
+
+
+# Procesado de imágenes
+
+
+grep  '!' ebook-temp/ebook.md  | grep 'http' | tr '(?' '\n\n' | grep 'http' | tr ')' ' '  > $TEMP_ImagesFile
+sed  's/http/wget http/' <$TEMP_ImagesFile > $TEMP_DownloadImagesFile
+
+cat $TEMP_DownloadImagesFile |  rev |  cut -d / -f 1 | rev >$TEMP_DownloadImagesNames
+
+# chmod u+x $TEMP_DownloadImagesFile
+# mkdir $TEMP_DirImages
+# cd $TEMP_DirImages
+# ../../$TEMP_DownloadImagesFile
+# cd ..
+
+# Generamos el ebook
+
 pandoc -o $TEMP_DIR/Raspberry.epub $TEMP_file
 
 mv $TEMP_DIR/Raspberry.epub .
-# rm -rf $TEMP_DIR
+
+
+# rm -rf $TEMP_DIR # comentado para comprobar

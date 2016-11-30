1ec748fe2343f633e011ce8ca1a438b379f3d839
javacasm <javacasm@gmail.com>
2016-11-30 23:20:56 +0100

    busca caracter

---
+++ codigo/buscaCaracter.py
@@ -0,0 +1,11 @@
+word= 'palabra';
+caracter = 'a';
+contador=0;
+mensaje='No se ha encontrado el caracter :('
+for i in range(len(word)):
+  if (word[i]==caracter):
+    mensaje='se ha encontrado el caracter!!!';
+    contador=contador+1;
+
+print mensaje;
+print 'Se encontrado '+str(contador)+' veces';

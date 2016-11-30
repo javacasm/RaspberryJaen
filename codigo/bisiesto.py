d7ae73bdd72dcf15d38453ef6606171199809ab7
javacasm <javacasm@gmail.com>
2016-11-30 23:51:12 +0100

    comentarios

--- codigo/bisiesto.py
+++ codigo/bisiesto.py
@@ -1,5 +1,5 @@
+# Programa que determina si un año es o no bisiesto
 year = input('Introduzca el anio: ');
-
 if ((year%400)==0  or (year % 100) ==0 or (year%4)==0):
   print 'Es bisiesto!!';
 else:
